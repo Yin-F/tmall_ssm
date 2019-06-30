decd70296c2b856623d8d1f6bb513a17fcb77c5c
YinF <1947434555@qq.com>
2019-07-01 00:16:30 +0800

    后台管理分页重构完成，分类逆向工程未开始，即CategoryMapper.xml、CategoryMapper.java、Category.java未自动生成和覆盖原创建的文件

---
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package com.how2java.tmall.mapper;
+
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.util.Page;
+
+import java.util.List;
+
+public interface CategoryMapper {
+   // List<Category> list();
+   //public List<Category> list(Page page);
+   List<Category> list();
+
+    //public int total();
+
+    void add(Category category);
+
+    void delete(int id);
+
+    Category get(int id);
+
+    void update(Category category);
+}
