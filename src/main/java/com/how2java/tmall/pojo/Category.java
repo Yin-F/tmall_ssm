c01cc7f7b5fe04afc6fd9946f0c6299d9f17b3d9
YinF <1947434555@qq.com>
2019-07-01 09:33:57 +0800

    完成所有逆向工程，即mapper、pojo、mapper。xml自动生成和覆盖原创建的文件

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -1,23 +1,23 @@
 package com.how2java.tmall.pojo;
 
 public class Category {
-        private Integer id;
+    private Integer id;
 
-        private String name;
+    private String name;
 
-        public Integer getId() {
-            return id;
-        }
+    public Integer getId() {
+        return id;
+    }
 
-        public void setId(Integer id) {
-            this.id = id;
-        }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName(String name) {
-            this.name = name == null ? null : name.trim();
-        }
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
 }
--- src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.how2java.tmall.service.impl;
 
 import com.how2java.tmall.mapper.CategoryMapper;
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.CategoryExample;
 import com.how2java.tmall.service.CategoryService;
 import com.how2java.tmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,18 @@ public class CategoryServiceImpl implements CategoryService {
     }
     */
 
+    /*
     @Override
     public List<Category> list() {
         return categoryMapper.list();
+    }
+    */
+    @Override
+    //传递一个example对象，这个对象指定按照id倒排序来查询
+    public List<Category> list() {
+        CategoryExample example =new CategoryExample();
+        example.setOrderByClause("id desc");
+        return categoryMapper.selectByExample(example);
     }
 
     /*
@@ -37,6 +47,8 @@ public class CategoryServiceImpl implements CategoryService {
         return categoryMapper.total();
     }
     */
+
+    /*
     @Override
     public void add(Category category) {
         categoryMapper.add(category);
@@ -55,5 +67,26 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public void update(Category category) {
         categoryMapper.update(category);
+    }
+    */
+
+    @Override
+    public void add(Category category) {
+        categoryMapper.insert(category);
+    }
+
+    @Override
+    public void delete(int id) {
+        categoryMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public Category get(int id) {
+        return categoryMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(Category category) {
+        categoryMapper.updateByPrimaryKeySelective(category);
     }
 }
